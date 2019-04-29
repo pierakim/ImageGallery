1fbeb2ed9ba83faa5166bcac81e3dce42449ce0f
pierakim.lambda <pierakim.lambda@gmail.com>
2019-04-29 21:57:58 +1000

    Replace BusService web app (why!) with a ServiceBus Class Library

---
+++ src/ImageGallery.ServiceBus/ServiceBus.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageGallery.ServiceBus
+{
+    public class ServiceBus : IHostedService
+    {
+        private readonly IBusControl _busControl;
+
+        public ServiceBus(IBusControl busControl)
+        {
+            _busControl = busControl;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return _busControl.StartAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return _busControl.StopAsync(cancellationToken);
+        }
+    }
+}
